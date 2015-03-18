b4799b5a4ca83254a6ad52e8aa78d1ded4b31ed7
Igor Cheliadinski <igor.cheliadinski@icloud.com>
2015-03-19 01:46:07 +0300

    Fixed task 6

--- HT_6/js/main.js
+++ HT_6/js/main.js
@@ -8,28 +8,59 @@ String.prototype.insert = function (index, string) {
   else
     return string + this;
 };
-changeButton.onclick = function () {
-	var text = stringInput.value;
-	for (var i = 0; i < text.length; i++) {
-		if (/[,.!?]/.test(text[i]) && (i !== text.length - 1)) {
-			if (text[i + 1] !== ' ') {
-				text = text.insert(i + 1, ' ');
-			};
-		};
-	};
+changeButton.onclick = change;
+
+// function change() {
+// 	var text = stringInput.value,
+// 			i;
+// 	for (i = 0; i < text.length; i++) {
+// 		if (/[,.!?]/.test(text[i]) && (i !== text.length - 1)) {
+// 			if (text[i + 1] !== ' ') {     
+// 				 text = text.insert(i + 1, ' ');
+// 			};
+// 		};
+// 	};
+// 	stringOutput.value = text.split(' ').map(function (value, index, arr) {
+// 		var newStr = '';
+// 		if (/[,.!?]/.test(value[value.length - 1])) {
+// 			for (i = value.length - 2; i >= 0; i--) {
+// 				newStr += value[i];
+// 			}
+// 			newStr += value[value.length - 1];
+// 			return newStr;
+// 		} else {
+// 			for (i = value.length - 1; i >= 0; i--) {
+// 				newStr += value[i];
+// 			}
+// 			return newStr;
+// 		}
+// 	}).join(' ');
+// }
+
+function change() {
+	var text = stringInput.value,
+			splittedText = text.split(' '),
+			word,
+			words,
+			i;
+
 	stringOutput.value = text.split(' ').map(function (value, index, arr) {
-		var newStr = '';
-		if (/[,.!?]/.test(value[value.length - 1])) {
-			for (var i = value.length - 2; i >= 0; i--) {
-				newStr += value[i];
-			}
-			newStr += value[value.length - 1];
-			return newStr;
+		if (!/[,.!?\]\[%*\(\)\"\'\:]/g.test(value)) {
+			return value.split('').reverse().join('');
 		} else {
-			for (var i = value.length - 1; i >= 0; i--) {
-				newStr += value[i];
+			word = '';
+			words = [];
+			for (i = 0; i < value.length; i++) {
+				if (/[,.!?\]\[%*\(\)\"\'\:]/g.test(value[i])) {
+					word = word.split('').reverse().join('');
+					word += value[i];
+					words.push(word);
+					word = '';
+				} else {
+					word += value[i];
+				}
 			}
-			return newStr;
+			return words.join('');
 		}
 	}).join(' ');
 }
